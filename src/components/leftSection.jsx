3ed13acc7c1f1a49c7cb4a99f00e0b4057836754
Andy Liu <aliu927@gmail.com>
2021-09-16 23:48:17 -0700

    added json file, added toggle on leftsection

--- src/components/leftSection.jsx
+++ src/components/leftSection.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react/cjs/react.development";
+
 function LeftSection(){
+  const [show, setShow] = useState(false);
   return (
   <div className="left_section_Container">
-    <button>Dropdown</button>
-    <div>
-      <a href="">Home</a>
-      <a href="">About</a>
-      <a href="">Contact</a>
-    </div>
+    <a href="#all" data-toggle="dropdown" className="dropdown_menu" onClick={()=>setShow(!show)}>All</a>
+    {/* {inline style online 9, however instead we used tenery operator to display either block or none.
+          to do that, we used hooks on line 4 to change state. Default state is set to false.} */}
+    <ul className="dropdown_subMenu" style={{display: show ? 'block': 'none'}}> 
+      <hr /><a href="#inbox">Inbox</a>
+      <hr /><a href="#draft">Draft</a>
+      <hr /><a href="#sent">Sent</a>
+      <hr /><a href="#deleted">Deleted</a>
+    </ul>
   </div>
   )
 }
---
+++ src/components/sub components/inbox.jsx
@@ -0,0 +1,5 @@
+function InboxComponent(){
+
+}
+
+export default InboxComponent;
