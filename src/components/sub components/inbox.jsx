8b54073ded5a26f62d19d97321be93b23cce7080
Andy Liu <aliu927@gmail.com>
2021-09-19 23:50:38 -0700

    learning fetch / then

--- src/components/sub components/inbox.jsx
+++ src/components/sub components/inbox.jsx
@@ -4,10 +4,13 @@ function InboxComponent(event){
   fetch("./emails.json")
   .then(response =>{
     console.log(response);
+    console.log(response.ok)
     return response.json();
   })
   .then(data =>{
     console.log(data);
+    console.log(data.listOfEmails)
+    console.log(data.listOfEmails[0].nameOfSender)
   })
 }
 
