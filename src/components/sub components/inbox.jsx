c6cd215aa63e213489c1371d79c717cb229e58b0
Andy Liu <aliu927@gmail.com>
2021-10-02 00:27:49 -0700

    Building out email layout

--- src/components/sub components/inbox.jsx
+++ src/components/sub components/inbox.jsx
@@ -6,9 +6,14 @@ return(
   <table style={{display: props.showTable.showTable ? "block":"none"}}>
     <tbody>
       <tr className="table_row_data">
-        {emailObjects.map((element,index) =>(
-          <td key={index}>{element.nameOfSender}</td>
-          ))}
+        {emailObjects.map((element,index) =>{
+          return(
+            <div className="table_data_emailObj">
+              <td>{element.nameOfSender}</td>
+              <td>{element.titleOfEmail}</td>
+            </div>
+          )
+        })}
       </tr>
     </tbody>
   </table>
