a32fb52e69898246d9adf470e4a850538d2c9c25
Andy Liu <aliu927@gmail.com>
2021-10-03 23:23:11 -0700

    updated middleSection

--- src/components/sub components/emailObject.jsx
+++ src/components/sub components/emailObject.jsx
@@ -3,6 +3,7 @@ export const emailObjects = [
     profilepicture:"",
     nameOfSender:"Kairon Shea",
     titleOfEmail:"Microsoft and Apple strategic partnership",
+    sentencePreview: "Are you ready for your big day?",
     bodyMessage:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor nec feugiat nisl pretium fusce id. Est velit egestas dui id. Aliquam nulla facilisi cras fermentum odio. Congue quisque egestas diam in arcu cursus euismod. Enim sed faucibus turpis in eu. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Turpis massa tincidunt dui ut. Tellus in metus vulputate eu scelerisque. Nisl tincidunt eget nullam non. Nisi scelerisque eu ultrices vitae auctor eu augue. Nibh sed pulvinar proin gravida hendrerit lectus a.",
     dateOfMessage:"",
     isAttachment:""
@@ -11,6 +12,7 @@ export const emailObjects = [
     profilepicture:"",
     nameOfSender:"Dianna Hackett",
     titleOfEmail:"Regarding your Department needs",
+    sentencePreview: "The advanvecd technology is what sold me.",
     bodyMessage:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor nec feugiat nisl pretium fusce id. Est velit egestas dui id. Aliquam nulla facilisi cras fermentum odio. Congue quisque egestas diam in arcu cursus euismod. Enim sed faucibus turpis in eu. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Turpis massa tincidunt dui ut. Tellus in metus vulputate eu scelerisque. Nisl tincidunt eget nullam non. Nisi scelerisque eu ultrices vitae auctor eu augue. Nibh sed pulvinar proin gravida hendrerit lectus a.",
     dateOfMessage:"",
     isAttachment:""
@@ -19,6 +21,7 @@ export const emailObjects = [
     profilepicture:"",
     nameOfSender:"Margo Brewer",
     titleOfEmail:"Andy, can you point me in the right direction?",
+    sentencePreview: "How would you like to do us a very big favor?",
     bodyMessage:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor nec feugiat nisl pretium fusce id. Est velit egestas dui id. Aliquam nulla facilisi cras fermentum odio. Congue quisque egestas diam in arcu cursus euismod. Enim sed faucibus turpis in eu. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Turpis massa tincidunt dui ut. Tellus in metus vulputate eu scelerisque. Nisl tincidunt eget nullam non. Nisi scelerisque eu ultrices vitae auctor eu augue. Nibh sed pulvinar proin gravida hendrerit lectus a.",
     dateOfMessage:"",
     isAttachment:""
@@ -27,6 +30,7 @@ export const emailObjects = [
     profilepicture:"",
     nameOfSender:"Keelan Griffin",
     titleOfEmail:"Can you spare 5 minutes, Andy?",
+    sentencePreview: "Thank you for finishing the last slice of pizza!",
     bodyMessage:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor nec feugiat nisl pretium fusce id. Est velit egestas dui id. Aliquam nulla facilisi cras fermentum odio. Congue quisque egestas diam in arcu cursus euismod. Enim sed faucibus turpis in eu. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Turpis massa tincidunt dui ut. Tellus in metus vulputate eu scelerisque. Nisl tincidunt eget nullam non. Nisi scelerisque eu ultrices vitae auctor eu augue. Nibh sed pulvinar proin gravida hendrerit lectus a.",
     dateOfMessage:"",
     isAttachment:""
@@ -35,6 +39,7 @@ export const emailObjects = [
     profilepicture:"",
     nameOfSender:"Odin Albert",
     titleOfEmail:"Microsoft and Apple strategic partnership",
+    sentencePreview: "This company is listed on the Paris stock exchange.",
     bodyMessage:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor nec feugiat nisl pretium fusce id. Est velit egestas dui id. Aliquam nulla facilisi cras fermentum odio. Congue quisque egestas diam in arcu cursus euismod. Enim sed faucibus turpis in eu. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Turpis massa tincidunt dui ut. Tellus in metus vulputate eu scelerisque. Nisl tincidunt eget nullam non. Nisi scelerisque eu ultrices vitae auctor eu augue. Nibh sed pulvinar proin gravida hendrerit lectus a.",
     dateOfMessage:"",
     isAttachment:""
@@ -42,7 +47,8 @@ export const emailObjects = [
   {
     profilepicture:"",
     nameOfSender:"Aedan Puckett",
-    titleOfEmail:"",
+    titleOfEmail:"Yo!",
+    sentencePreview: "I’m in the middle, I don’t know what to decide.",
     bodyMessage:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor nec feugiat nisl pretium fusce id. Est velit egestas dui id. Aliquam nulla facilisi cras fermentum odio. Congue quisque egestas diam in arcu cursus euismod. Enim sed faucibus turpis in eu. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Turpis massa tincidunt dui ut. Tellus in metus vulputate eu scelerisque. Nisl tincidunt eget nullam non. Nisi scelerisque eu ultrices vitae auctor eu augue. Nibh sed pulvinar proin gravida hendrerit lectus a.",
     dateOfMessage:"",
     isAttachment:""
@@ -51,6 +57,7 @@ export const emailObjects = [
     profilepicture:"",
     nameOfSender:"Jaheim Emery",
     titleOfEmail:"Andy - any thoughts?",
+    sentencePreview: "If you could go anywhere on vacation, where would you go?",
     bodyMessage:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor nec feugiat nisl pretium fusce id. Est velit egestas dui id. Aliquam nulla facilisi cras fermentum odio. Congue quisque egestas diam in arcu cursus euismod. Enim sed faucibus turpis in eu. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Turpis massa tincidunt dui ut. Tellus in metus vulputate eu scelerisque. Nisl tincidunt eget nullam non. Nisi scelerisque eu ultrices vitae auctor eu augue. Nibh sed pulvinar proin gravida hendrerit lectus a.",
     dateOfMessage:"",
     isAttachment:""
--- src/components/sub components/inbox.jsx
+++ src/components/sub components/inbox.jsx
@@ -3,14 +3,16 @@ import { emailObjects } from "./emailObject";
 function InboxComponent(props){
   console.log(props.showTable.showTable)
 return(
-  <table style={{display: props.showTable.showTable ? "block":"none"}}>
-    <tbody>
+  <table className="table_Container"style={{display: props.showTable.showTable ? "block":"none"}}>
+    <tbody className="table_body">
       <tr className="table_row_data">
         {emailObjects.map((element,index) =>{
           return(
             <div className="table_data_emailObj">
               <td>{element.nameOfSender}</td>
-              <td>{element.titleOfEmail}</td>
+              <td><strong>{element.titleOfEmail}</strong></td>
+              <td>{element.sentencePreview}</td>
+              <hr />
             </div>
           )
         })}
