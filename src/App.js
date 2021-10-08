c7f96a021e57b2deaf3e56b284fdf987da12ed3d
Andy Liu <aliu927@gmail.com>
2021-10-07 23:43:02 -0700

    figuring out display rightSection

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import RightSection from "./components/rightSection";
 
 function App() {
   const [showTable, toggleTable] = useState(false);
+  //const [showEmail, displayEmail = useState(0);
 
   return (
     <div className="App">
       <LeftSection toggleTable={toggleTable} />
       <MiddleSection showTable={showTable} />
-      <RightSection />
+      <RightSection /* Pass something here / Recv Something here */ />
     </div>
   );
 }
