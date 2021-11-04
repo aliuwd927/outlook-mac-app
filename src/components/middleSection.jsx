cc8e204787deb527aaee4235885935e25d6dfe1a
Andy Liu <aliu927@gmail.com>
2021-11-03 23:59:07 -0700

    updated for readability

--- src/components/middleSection.jsx
+++ src/components/middleSection.jsx
@@ -3,7 +3,7 @@ import InboxComponment from "./sub components/inbox";
 function MiddleSection(props){  
   return (
     <div className="middle_section_Container">
-      <InboxComponment showTable = {props} emailClick = {props}/>
+      <InboxComponment {...props}/>
     </div>
   )
 }
--- src/components/sub components/inbox.jsx
+++ src/components/sub components/inbox.jsx
@@ -1,14 +1,13 @@
 import { emailObjects } from "./emailObject";
 
 function InboxComponent(props){
- 
 return(
-  <table className="table_Container"style={{display: props.showTable.showTable ? "block":"none"}}>
+  <table className="table_Container"style={{display: props.showTable ? "block":"none"}}>
     <tbody className="table_body">
       <tr className="table_row_data">
         {emailObjects.map((element,index) =>{
           return(
-            <div className="table_data_emailObj" onClick={()=>props.emailClick.emailClicked(index)} >
+            <div className="table_data_emailObj" onClick={()=>props.emailClicked(index)} >
               <td>{element.nameOfSender}</td>
               <td><strong>{element.titleOfEmail}</strong></td>
               <td>{element.sentencePreview}</td>
