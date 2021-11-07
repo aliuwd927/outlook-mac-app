412acea72f2c2915a9677e79a2df726fdc911084
Andy Liu <aliu927@gmail.com>
2021-11-07 12:06:45 -0800

    rightSection now displays correct email & renders.

--- src/components/rightSection.jsx
+++ src/components/rightSection.jsx
@@ -4,7 +4,20 @@ function RightSection(props){
  
   return (
   <div className="right_section_Container">
-    {/* {props.showEmail} */}
+    {emailObjects.map((element,index) => {
+      if(props.showEmail === index){
+        return (
+          <div className="right_display_emailObj">
+              <td>{element.nameOfSender}</td>
+              <td><strong>{element.titleOfEmail}</strong></td>
+              <td>{element.sentencePreview}</td>
+              <hr />
+            </div>
+        )
+      }else{
+        return null;
+      }
+    })}
     
   </div>
   )
