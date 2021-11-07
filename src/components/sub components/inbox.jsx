0a8074d5c37d41e0f8c895fda7cc0ade158e480e
Andy Liu <aliu927@gmail.com>
2021-11-07 00:04:21 -0700

    render conditional

--- src/components/sub components/inbox.jsx
+++ src/components/sub components/inbox.jsx
@@ -7,7 +7,7 @@ return(
       <tr className="table_row_data">
         {emailObjects.map((element,index) =>{
           return(
-            <div className="table_data_emailObj" onClick={()=>props.emailClicked(index)} >
+            <div className="table_data_emailObj" onClick={()=>{props.emailClicked(index)}} >
               <td>{element.nameOfSender}</td>
               <td><strong>{element.titleOfEmail}</strong></td>
               <td>{element.sentencePreview}</td>
